7dee7bbf432f6a15ba5ccd558cb927edb62abf3b
Nolan Heimlich <76967780+nheimlich@users.noreply.github.com>
2026-02-15 20:19:46 -0500

    feat(ruleset): adding ruleset, tofu updates
    Signed-off-by: Nolan Heimlich <76967780+nheimlich@users.noreply.github.com>

--- tofu/main.tf
+++ tofu/main.tf
@@ -15,7 +15,7 @@ resource "github_repository" "homelab" {
   visibility                  = "public"
   allow_merge_commit          = false
   allow_rebase_merge          = false
-  allow_update_branch         = true
+  allow_update_branch         = false
   allow_squash_merge          = true
   delete_branch_on_merge      = true
   has_issues                  = false
@@ -23,7 +23,33 @@ resource "github_repository" "homelab" {
   has_wiki                    = false
   homepage_url                = "https://k8s.nhlabs.org"
   web_commit_signoff_required = true
-  allow_forking               = false
-  squash_merge_commit_title   = "PR_TITLE"
-  squash_merge_commit_message = "PR_BODY"
+  squash_merge_commit_title   = "COMMIT_OR_PR_TITLE"
+  squash_merge_commit_message = "COMMIT_MESSAGES"
+}
+
+resource "github_repository_ruleset" "main_protection" {
+  name        = "homelab-protection"
+  repository  = "homelab"
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["refs/heads/main"]
+      exclude = []
+    }
+  }
+
+  bypass_actors {
+    actor_id    = 5
+    actor_type  = "RepositoryRole"
+    bypass_mode = "always"
+  }
+
+  rules {
+    deletion                = true
+    non_fast_forward        = true
+    required_linear_history = true
+    required_signatures     = true
+  }
 }
